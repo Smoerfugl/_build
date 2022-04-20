9efb8e31258dc08e24f2ce0e2729a2fa6600c1c4
Chris Justesen <chris@smoerfugl.dk>
2022-04-20 14:27:09 +0200

    Lwoercase tyeservice name

--- _build/_build/Tye/TyeService.cs
+++ _build/_build/Tye/TyeService.cs
@@ -4,14 +4,22 @@ public class TyeService
 {
     public TyeService()
     {
-        
     }
+
     public TyeService(string name, string project)
     {
         Name = name;
         Project = project;
     }
-    public string Name { get; set; } = null!;
+
+    private string _name = "";
+
+    public string Name
+    {
+        get => _name;
+        set => _name = value.ToLower();
+    }
+
     public string Project { get; set; } = null!;
     public int? Replicas { get; set; } = 1;
     public List<EnvironmentVariable> Env { get; set; } = new();
