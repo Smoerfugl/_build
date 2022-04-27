c7de7e7721b6ab57aafe975ba029476e293b8b49
Chris Justesen <chris@smoerfugl.dk>
2022-04-27 23:37:41 +0200

    Add limits to pipeline

--- _build/_build/Pipelines/Pipeline.cs
+++ _build/_build/Pipelines/Pipeline.cs
@@ -8,13 +8,45 @@ public class Pipeline
     public List<PipelineService> Services { get; set; } = new();
     public Dictionary<string, List<EnvironmentVariable>> EnvironmentVariables { get; set; } = new();
     public string Name { get; set; } = null!;
+
+    public string GetNamespace(Env env)
+    {
+        return $"{env.ToString().ToLower()}-{Name}";
+    }
 }
 
 public class PipelineService
 {
     public string Name { get; set; } = null!;
-    public int? ServicePort { get; set; }
+    public int ServicePort { get; set; }
     public int? Replicas { get; set; } = 1;
     public string? Hostname { get; set; }
     public string Project { get; set; } = null!;
+
+    public ServiceResources Resources { get; set; } = new ServiceResources()
+    {
+        Limits = new Dictionary<ResourceUnits, string>()
+        {
+            { ResourceUnits.CPU, "250m" },
+            { ResourceUnits.Memory, "64Mi" }
+        },
+        Requests = new Dictionary<ResourceUnits, string>()
+        {
+            { ResourceUnits.CPU, "500m" },
+            { ResourceUnits.Memory, "256Mi" }
+        }
+    };
+
+}
+
+public class ServiceResources
+{
+    public Dictionary<ResourceUnits, string> Limits { get; set; } = new();
+    public Dictionary<ResourceUnits, string> Requests { get; set; } = new();
+}
+
+public enum ResourceUnits
+{
+    CPU,
+    Memory
 }
