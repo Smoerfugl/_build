a7753eb8d902593a9be5b5ee3595ea10872de984
Chris Justesen <chris@smoerfugl.dk>
2022-04-27 17:34:25 +0200

    Add GenerateManifesto

--- _build/_build/Kubernetes/GenerateManifesto.cs
+++ _build/_build/Kubernetes/GenerateManifesto.cs
@@ -1,19 +1,23 @@
-using Build.Tye;
+using Build.Pipelines;
+using Build.Environments;
 
 namespace Build.Kubernetes;
 
 public interface IGenerateManifesto
 {
-    List<object> Invoke(TyeConfig tyeConfig);
+    List<object> Invoke(Pipeline pipeline, Env env, string domainValue);
 }
 
 public class GenerateManifesto : IGenerateManifesto
 {
-    public List<object> Invoke(TyeConfig tyeConfig)
+    public List<object> Invoke(Pipeline pipeline, Env env, string domainValue)
     {
+        var routes = new GenerateIngressRoutesRoutesList().Invoke(pipeline, domainValue);
         var objects = new List<object>
         {
-            new GenerateNamespace(tyeConfig.Namespace).Invoke()
+            new GenerateNamespace(pipeline, env).Invoke(),
+            routes,
+            new GenerateCertificates().Invoke(routes),
         };
         return objects;
     }
