23af763e4e94652b56c36e613779c025e6ef493f
Chris Justesen <chris@smoerfugl.dk>
2022-05-01 21:29:19 +0200

    Fix annotations in Converter

--- _build/_build.Tests/Docker/Converter.cs
+++ _build/_build.Tests/Docker/Converter.cs
@@ -7,24 +7,21 @@ namespace _build.Tests.Docker;
 
 public class Converter : TextWriter
 {
-    ITestOutputHelper _output;
+    readonly ITestOutputHelper _output;
 
     public Converter(ITestOutputHelper output)
     {
         _output = output;
     }
 
-    public override Encoding Encoding
-    {
-        get { return Encoding.UTF8; }
-    }
+    public override Encoding Encoding => Encoding.UTF8;
 
-    public override void WriteLine(string message)
+    public override void WriteLine(string? message)
     {
         _output.WriteLine(message);
     }
 
-    public override void WriteLine(string format, params object[] args)
+    public override void WriteLine(string format, params object?[] args)
     {
         _output.WriteLine(format, args);
     }
